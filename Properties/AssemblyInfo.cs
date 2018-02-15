d5208e59019b1d134623099b89b3372393eaa4f6
Greg D <gdivis@inedo.com>
2018-02-15 12:30:55 -0500

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Inedo.BuildMaster.Extensibility;
+
+[assembly: AssemblyTitle("Mercurial")]
+[assembly: AssemblyDescription("Legacy source control integration for Mercurial.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyProduct("BuildMaster")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2018")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: CLSCompliant(false)]
+[assembly: RequiredBuildMasterVersion("5.6.0")]
